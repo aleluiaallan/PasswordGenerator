767f50aea382b84dfa4ed73848c7705f8db33990
Allan Aleluia <allan.souza92@hotmail.com>
2023-04-13 17:46:15 +0100

    PasswordGenerator

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string characters = "ABCDEFGHIJKLMNOPQRSTUVXYZ" +
+                "ABCDEFGHIJKLMNOPQRSTUVXYZ".ToLower() +
+                "0123456789" +
+                "!@#$%¨&*()_-+=,.<>;:?/|~^´`{}ªº[]";
+
+            Console.WriteLine("Quantos digitos sua senha precisa?");
+            int digitos = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+            int sorted = 0;
+            StringBuilder password = new StringBuilder();
+            for(int i = 0; i < digitos; i++)
+            {
+                sorted=random.Next(0,characters.Length -1);
+                password.Append(characters[sorted]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Senha gerada:");
+            Console.WriteLine(password);
+            Console.ReadKey();
+
+            
+            
+
+        }
+    }
+}
